fb1319151d060102eb218ed764382885f591b99a
nihal-ansari <nihal.ansari@in.ibm.com>
2018-09-17 02:18:06 +0530

    first commit

---
+++ declaration.js
@@ -0,0 +1,2 @@
+exports.ccid =  'fabpoebr';
+exports.hostIP =  '192.168.99.100';
---
+++ upgradecc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error
+set -e
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+starttime=$(date +%s)
+
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n fabpoebr -v 2.0 -p github.com/fabpoe
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode upgrade -o orderer.example.com:7050 -C mychannel -n fabpoebr -v 2.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+sleep 10
+
+
+
+printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
+printf "Start by installing required packages run 'npm install'\n"
+printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n"
+printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n"
+printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
